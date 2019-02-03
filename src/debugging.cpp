b0685adb0ab28217583d1cbfd9b072baca825d99
Gabriel Pulido <autodeskgato@gmail.com>
2019-02-03 06:37:12 -0800

    Removed extra debugging function

--- src/debugging.cpp
+++ src/debugging.cpp
@@ -5,15 +5,3 @@ void debug(std::string text) {
   printf(text.c_str());
   pros::delay(500);
 }
-
-
-
-//Diagnostic Logger - Will log data to a file.
-void log(const std::string& text) {
-  FILE* diagnostics;
-  static FILE* diagnostics;
-  static bool diagnosticsFileOpenFailure;
-  if(!diagnostics) {
-    diagnostics = fopen("/usd/diagonal-stick", "w");
-  }
-}
