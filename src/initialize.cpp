285c6c4c2a9b64aad35507b41f5822ff2c46a279
Gabriel Pulido <autodeskgato@gmail.com>
2019-02-05 17:41:57 -0800

    Stall after fatal initialization error

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -8,7 +8,13 @@ void initialize() {
     getRobot().beginTasks();
   } catch(const std::exception& e) {
     std::cout << "Fatal initialization error: " + std::string(e.what()) << std::endl;
+    while(true) {
+      pros::delay(1000);
+    }
   } catch(...) {
     std::cout << "Unknown fatal initialization error." << std::endl;
+    while(true) {
+      pros::delay(1000);
+    }
   }
 }
