1f5a55f4e44b319fae3fde7fb79f9b1110f8028b
Gabriel Pulido <autodeskgato@gmail.com>
2019-02-11 20:06:16 -0800

    add default value

--- src/autonomous.hpp
+++ src/autonomous.hpp
@@ -6,7 +6,7 @@
 using json = nlohmann::json;
 void runAuton(json::iterator loc, json::iterator end, bool isBlue);
 void runAutonNamed(std::string name, bool isBlue);
-void moveToSetpoint(RoboPosition pt, double velLimit, bool stayStraight, int extraTime);
+void moveToSetpoint(RoboPosition pt, double velLimit, bool stayStraight, int extraTime, int turnExtraTime = 0);
 void setBlue(bool);
 bool getBlue();
 void runMotion(json motionObject, RoboPosition& lastPos, bool isBlue);
