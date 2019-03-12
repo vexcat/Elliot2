422bead9b35ccbfdb60eba8ae7faa5aae6386f8c
Gabriel Pulido <autodeskgato@gmail.com>
2019-03-11 17:31:26 -0700

    Move declaration of selectOption

--- src/display.hpp
+++ src/display.hpp
@@ -3,4 +3,3 @@
 std::string getSelectedAuton();
 void startupDisplay();
 void line_set(int line, std::string str);
-int selectOption(std::initializer_list<std::string> list, int idx);
--- src/editors.hpp
+++ src/editors.hpp
@@ -2,3 +2,4 @@
 
 double editNumber(double number, int fix);
 std::string editString(std::string text);
+int selectOption(std::initializer_list<std::string> list, int idx);
