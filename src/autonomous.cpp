7708b2dd22e1d65b01bef31748d9561afcaf4fe0
Gabriel Pulido <autodeskgato@gmail.com>
2019-03-23 09:04:18 -0700

    Document autonomous.cpp/.hpp

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file autonomous.cpp
+ * 
+ * This file interprets JSON objects as individual steps of an autonomous.
+ */
+
 #include "main.h"
 #include "display.hpp"
 #include "autonomous.hpp"
@@ -8,16 +14,26 @@
 #include "pros/apix.h"
 using namespace std;
 
+///Whether to execute autonomous motions as on the blue side, or the red side.
 bool isBlue;
 void setBlue(bool blue) {
   isBlue = blue;
 }
-
 bool getBlue() {
   return isBlue;
 }
 
-//Ball tracking function
+/**
+ * Tracks a ball using the vision sensor.
+ * It steers the robot according to the nearest ball's x position,
+ * and stops once the nearest ball's y position is lower than oovThreshold.
+ * 
+ * @param maxVel       Velocity limit in [0, 1].
+ * @param threshold    How far a ball can travel side-to-side without steering the robot.
+ * @param oovThreshold How close a ball can be before stopping to intake.
+ * @param attack       How far to drive forward once the ball is close to the robot.
+ * @param extraTime    How long to wait for ball to finish intaking.
+ */
 void trackBall(double maxVel, double threshold, double oovThreshold, double attack, int extraTime) {
   auto &bot = getRobot();
   auto &cha = bot.box->base;
@@ -61,6 +77,7 @@ void trackBall(double maxVel, double threshold, double oovThreshold, double atta
   bot.intake.moveVelocity(0);
 }
 
+//Move to a point, documented in .hpp.
 void moveToSetpoint(RoboPosition pt, double velLimit, bool reverse, int extraTime, int turnExtraTime) {
   auto &bot = getRobot();
   auto &gps = bot.gps;
@@ -94,39 +111,52 @@ void moveToSetpoint(RoboPosition pt, double velLimit, bool reverse, int extraTim
 
 void runMotion(json motionObject, RoboPosition& offset, bool isBlue) {
   auto &bot = getRobot();
-  //get the type of motion
+  //Get the type of motion
   auto type = motionObject["type"].get<std::string>();
-  //now, run the appropriate function for each type.
+  //Now, run the appropriate function for each type.
+  //Move to point
   if(type == "position") {
     //Get the target position.
     RoboPosition target = {
-      bot.gps.inchToCounts(motionObject["x"].get<double>()) + offset.x,
-      bot.gps.inchToCounts(motionObject["y"].get<double>()) + offset.y,
+      bot.gps.inchToCounts(motionObject["x"].get<double>()) + offset.x, //X in inches
+      bot.gps.inchToCounts(motionObject["y"].get<double>()) + offset.y, //Y in inches
       0
     };
     //Now, apply the blue mode by reversing the target x.
     if(isBlue) target.x = bot.gps.inchToCounts(144) - target.x;
-    moveToSetpoint({
-      target.x,
-      target.y,
-      0
-    },
-    motionObject["v"].get<double>(),         //Max velocity
-    motionObject["r"].get<bool>(),           //Reverse? 
-    motionObject["t"].get<double>() * 1000,  //Extra straight time
-    motionObject["rT"].get<double>() * 1000);//Extra turn time
+    //Move to point target
+    moveToSetpoint(
+      {
+        target.x,
+        target.y,
+        0
+      },
+      motionObject["v" ].get<double>(),        //Max velocity
+      motionObject["r" ].get<bool  >(),        //Reverse? 
+      motionObject["t" ].get<double>() * 1000, //Extra straight time
+      motionObject["rT"].get<double>() * 1000  //Extra turn time
+    );
   }
+  //Rotate to an absolute orientation
   if(type == "rotateTo") {
     double dTheta = motionObject["o"].get<double>();
+    //Add offset orientation
     dTheta += offset.o;
+    //Invert turn if on blue side
     dTheta = isBlue ? PI - dTheta : dTheta;
+    //Subtract part of turn already completed
     dTheta -= bot.gps.getPosition().o;
+    //Make periodically efficient, [-pi, pi] turns.
     dTheta = periodicallyEfficient(dTheta);
+    //"v": Velocity limit in [0, 1]
     double velLimit = motionObject["v"].get<double>();
     bot.box->base.setMaxVelocity(velLimit * (int)bot.left.getGearing());
+    //Turn by dTheta radians
     bot.box->base.turnAngle(dTheta * -(180 / PI) * okapi::degree);
+    //Wait extra "t" seconds
     pros::delay(motionObject["t"].get<double>() * 1000);
   }
+  //Call trackBall() defined above
   if(type == "autoball") {
     trackBall(
       motionObject["v"].get<double>(),
@@ -136,30 +166,38 @@ void runMotion(json motionObject, RoboPosition& offset, bool isBlue) {
       motionObject["t"].get<double>() * 1000
     );
   }
+  //Move puncher to low target
   if(type == "low") {
     bot.puncher.lowTarget();
     pros::delay(1000 * motionObject["t"].get<double>());
   }
+  //Move puncher to high target
   if(type == "high") {
     bot.puncher.highTarget();
     pros::delay(1000 * motionObject["t"].get<double>());
   }
+  //Punch ball
   if(type == "punch") {
     bot.puncher.shoot();
     pros::delay(1000 * motionObject["t"].get<double>());
   }
+  //Spin intake
   if(type == "intake") {
+    //"v": Velocity to move intake by in [-1, 1]
     double v = motionObject["v"].get<double>();
     bot.intake.controllerSet(v);
+    //If "t" is set, wait "t" seconds and stop the intake.
     double timing = motionObject["t"].get<double>();
     if(timing != 0) {
       pros::delay((int)(timing * 1000));
       bot.intake.controllerSet(0);
     }
   }
+  //Delay by "t" seconds
   if(type == "delay") {
     pros::delay((int)(1000 * motionObject["t"].get<double>()));
   }
+  //Brake Modes
   if(type == "hold") {
     bot. left.setBrakeMode(AbstractMotor::brakeMode::hold);
     bot.right.setBrakeMode(AbstractMotor::brakeMode::hold);
@@ -172,50 +210,69 @@ void runMotion(json motionObject, RoboPosition& offset, bool isBlue) {
     bot. left.setBrakeMode(AbstractMotor::brakeMode::brake);
     bot.right.setBrakeMode(AbstractMotor::brakeMode::brake);
   }
+  //Set GPS position
   if(type == "origin") {
-    //Offset does not respect blueMode, but the GPS position should.
+    //"x"/"y"/"o": X & Y in inches, orientation in radians.
     RoboPosition newPos = {
       bot.gps.inchToCounts(motionObject["x"].get<double>()),
       bot.gps.inchToCounts(motionObject["y"].get<double>()),
       motionObject["o"].get<double>()
     };
+    //Offset should start with an orientation of 0, always.
+    //This can be changed by the user with a "delta" command
+    //but is rarely needed.
     offset = {
       newPos.x,
       newPos.y,
       0
     };
+    //Offset does not respect blueMode, but the GPS position should.
     newPos.x = isBlue ? bot.gps.inchToCounts(144) - newPos.x : newPos.x;
     newPos.o = isBlue ? PI - newPos.o : newPos.o;
     bot.gps.setPosition(newPos);
   }
+  //Set just the offset
   if(type == "delta") {
+    //"x"/"y"/"o": X & Y in inches, orientation in radians.
     offset = {
       bot.gps.inchToCounts(motionObject["x"].get<double>()),
       bot.gps.inchToCounts(motionObject["y"].get<double>()),
       motionObject["o"].get<double>()
     };
   }
+  //Directly control base motors, without PID.
   if(type == "direct") {
     bot.box->base.stop();
+    //"l"/"r": Left & right velocities in [-1, 1]
     double l = motionObject["l"].get<double>() * (int)bot.left.getGearing();
     double r = motionObject["r"].get<double>() * (int)bot.right.getGearing();
+    //Respect isBlue by swapping l & r.
     bot.left.moveVelocity(isBlue ? r : l);
     bot.right.moveVelocity(isBlue ? l : r);
+    //If "t" is set: delay "t" seconds then stop base
     if(motionObject["t"].get<double>() != 0) {
       pros::delay(motionObject["t"].get<double>() * 1000);
       bot.left.moveVelocity(0);
       bot.right.moveVelocity(0);
     }
   }
+  //Arm motor control
   if(type == "arm") {
+    //"p": Position in degrees to move to, at 100rpm
     double position = motionObject["p"].get<double>();
     bot.arm.moveAbsolute(position, 100);
+    //"t": Wait time for arm to start moving
     pros::delay(motionObject["t"].get<double>() * 1000);
   }
+  //Straight Line
   if(type == "sline") {
+    //"d": Distance to travel in inches
     double distance = bot.gps.inchToCounts(motionObject["d"].get<double>());
+    //"v": Velocity limit in [0, 1]
     bot.box->base.setMaxVelocity(motionObject["v"].get<double>() * (int)bot.left.getGearing());
+    //Move distance
     bot.box->base.moveDistance(distance);
+    //"t": Extra time to wait after moving
     pros::delay(motionObject["t"].get<double>() * 1000);
   }
 }
@@ -245,6 +302,12 @@ void runAutonNamed(std::string name, bool isBlue) {
   printf("%s does not name an autonomous. Will stall.\n", name.c_str());
 }
 
+/**
+ * autonomous() is a function called by PROS when competition
+ * control state switches to autonomous mode. This implementation
+ * will call runAutonNamed() with the name of the auton selected
+ * on the brain screen's autonomous selector.
+ */
 void autonomous() {
   runAutonNamed(getSelectedAuton(), isBlue);
 }
--- src/autonomous.hpp
+++ src/autonomous.hpp
@@ -4,9 +4,58 @@
 #include "gps.hpp"
 #include <string>
 using json = nlohmann::json;
+
+/**
+ * Runs an auton from a given starting position in a list, to
+ * an end position.
+ * 
+ * @param loc    Index at the start of auton
+ * @param end    Index at the end of auton
+ * @param isBlue Whether to invert turns
+ * @see runMotion()
+ */
 void runAuton(json::iterator loc, json::iterator end, bool isBlue);
+
+/**
+ * Runs an auton saved on the SD card, by name.
+ * Auton is loaded from getState()["autons"][name].
+ * 
+ * @param name   Name of autonomous to run
+ * @param isBlue Whether to invert turns
+ * @see runAuton()
+ */
 void runAutonNamed(std::string name, bool isBlue);
+
+/**
+ * Moves to a point using odometry data. This will first turn
+ * towards the point, then move straight to it.
+ * 
+ * @param pt            Point to move to
+ * @param velLimit      Velocity limit in [0, 1]
+ * @param reverse       Whether to move straight to point in reverse/forward
+ * @param extraTime     Extra time to let straight motion finish
+ * @param turnExtraTime Extra time to let turn-to-face-point motion finish
+ */
 void moveToSetpoint(RoboPosition pt, double velLimit, bool stayStraight, int extraTime, int turnExtraTime = 0);
+
+/**
+ * Control whether autonomous motions will run as red or blue, inverting turns.
+ */
 void setBlue(bool);
+
+/**
+ * Check if turns are being inverted by the blue mode.
+ */
 bool getBlue();
-void runMotion(json motionObject, RoboPosition& lastPos, bool isBlue);
+
+/**
+ * Run a single JSON motion. The offset parameter can be used to run motions
+ * originally intended for a different area of the field, to tack together
+ * autonomi. The isBlue parameter overrides the global blue mode, to invert
+ * turns for the blue side.
+ * 
+ * @param motionObject JSON data to interpret
+ * @param offset       x, y, and o to be added to motion data
+ * @param isBlue       Whether to invert turns
+ */
+void runMotion(json motionObject, RoboPosition& offset, bool isBlue);
