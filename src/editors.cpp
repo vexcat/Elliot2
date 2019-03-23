d1597ae9c6605146ac54f8fd60971b0d63aaf416
Gabriel Pulido <autodeskgato@gmail.com>
2019-03-22 21:45:24 -0700

    Add proper docs to editors

--- src/editors.cpp
+++ src/editors.cpp
@@ -6,11 +6,15 @@
 //------------------------------------------------------------------------------------
 //  Controller Editors
 //  ------------------
-//  This file contains the functions for editing numbers & text on the V5 Controller's
-//  screen. It is used primarily in MotionEditor of display.cpp.
+//  This file defines the functions for editing numbers & text on the V5 Controller's
+//  screen. It is used throughout display.cpp to edit robot configuration & auton.
 //------------------------------------------------------------------------------------
 
-//Render the cursor arrows that show you what place you're editing
+/**
+ * Render cursor arrows to display place being edited.
+ * 
+ * @param cursor Index of place to display arrows from [0, 15)
+ */
 void renderEditorArrows(int cursor) {
   std::string arrowText(15, ' ');
   arrowText[cursor] = '^';
@@ -19,7 +23,14 @@ void renderEditorArrows(int cursor) {
   line_set(2, arrowText);
 }
 
-//Render a number to a certain amount of decimals, and the cursor
+/**
+ * Renders the number editor, called internally by editNumber().
+ * 
+ * @param number Number to display on-screen
+ * @param fix    Decimal places of precision to display number with
+ * @param cursor Index of place being edited
+ * @see editNumber()
+ */
 void renderNumberEditor(double number, int fix, int cursor) {
   char textData[16];
   snprintf(textData, 16, "%+014.*f\n", fix, number);
@@ -27,27 +38,36 @@ void renderNumberEditor(double number, int fix, int cursor) {
   renderEditorArrows(cursor);
 }
 
-//Render text and the cursor
+/**
+ * Renders the string editor, called internally by editString().
+ * 
+ * @param text   Text to display on-screen
+ * @param cursor Index in text being edited
+ * @see editString()
+ */
 void renderStringEditor(std::string& text, int cursor) {
   line_set(1, text);
   renderEditorArrows(cursor);
 }
 
-//Edit an existing number and return the result
+//Edit number using controller LCD & input
 double editNumber(double number, int fix) {
   auto &ctrl = getRobot().controller;
   int cursor = 14 - fix - (fix > 0);
   renderNumberEditor(number, fix, cursor);
   while(!ctrl.get_digital_new_press(DIGITAL_A) && !ctrl.get_digital_new_press(DIGITAL_B)) {
+    //Cursor Movement
     int hdir = getHorizontalDirection();
     cursor += hdir;
     bound(cursor, 15);
+    //(Skip the decimal place)
     if(14 - cursor == fix && fix != 0) {
       if(hdir < 0) cursor --;
       if(hdir > 0) cursor ++;
     }
     if(hdir) renderNumberEditor(number, fix, cursor);
 
+    //Place-value Addition/Subtraction
     int vdir = getVerticalDirection();
     if(vdir && cursor == 0) {
       number *= -1;
@@ -74,12 +94,18 @@ double editNumber(double number, int fix) {
       number = std::floor(number);
       renderNumberEditor(number, fix, cursor);
     }
+
+    //Loop delay
     pros::delay(5);
   }
   return number;
 }
 
-//Remove padding to the right of a string
+/**
+ * Removes trailing spaces in a string.
+ * 
+ * @param str Text to be trimmed
+ */
 void unrightPad(std::string& str) {
   //Count the spaces on the right of the string.
   int count = 0;
@@ -88,22 +114,25 @@ void unrightPad(std::string& str) {
   str.erase(str.end() - count, str.end());
 }
 
-//Edit an existing string and return the result
+//Edit text using controller LCD & input
 std::string editString(std::string text) {
   auto &ctrl = getRobot().controller;
   int cursor = 0;
   text.insert(text.size(), 15 - text.size(), ' ');
   renderStringEditor(text, cursor);
   while(!ctrl.get_digital_new_press(DIGITAL_A) && !ctrl.get_digital_new_press(DIGITAL_B)) {
+    //Cursor Movement
     int hdir = getHorizontalDirection();
     cursor += hdir;
     bound(cursor, 15);
     if(hdir) renderStringEditor(text, cursor);
 
+    //Symbol adjustment
     int vdir = getVerticalDirection();
     for(int i = 0; i < std::abs(vdir); i++) {
       auto &sym = text[cursor] += vdir > 0 ? 1 : -1;
 
+      //Forward adjustment
       if(vdir > 0) {
         if(sym == ' ' + 1) {
           sym = 'a';
@@ -119,6 +148,7 @@ std::string editString(std::string text) {
         }
       }
 
+      //Reverse adjustment
       if(vdir < 0) {
         if(sym == ' ' - 1) {
           sym = '9';
@@ -135,34 +165,41 @@ std::string editString(std::string text) {
       }
     }
 
-    //Just in case
+    //Just in case, replace any unwanted symbols with spaces
     auto &sym = text[cursor];
     if(!(('a' <= sym && sym <= 'z') || ('A' <= sym && sym <= 'Z') || ('0' <= sym && sym <= '9') || sym == ' ')) {
       sym = ' ';
     }
 
+    //Re-render if symbol was changed
     if(vdir) renderStringEditor(text, cursor);
 
+    //Loop delay
     pros::delay(5);
   }
   unrightPad(text);
   return text;
 }
 
-//Select an option out of a list
+//Select option from list using controller LCD & input
 int selectOption(std::initializer_list<std::string> list, int idx) {
   auto &ctrl = getRobot().controller;
   renderEditorArrows(0);
   line_set(1, list.begin()[idx]);
   while(true) {
+    //Option Changing
     int vdir = getVerticalDirection();
     idx += vdir;
     bound(idx, list.size());
     if(vdir) line_set(1, list.begin()[idx]);
-    pros::delay(5);
+
+    //Option Selection
     if(ctrl.get_digital_new_press(DIGITAL_B) || ctrl.get_digital_new_press(DIGITAL_A)) {
       break;
     }
+
+    //Loop delay
+    pros::delay(5);
   }
   return idx;
 }
--- src/editors.hpp
+++ src/editors.hpp
@@ -1,5 +1,34 @@
 #pragma once
 
+//------------------------------------------------------------------------------------
+//  Controller Editors
+//  ------------------
+//  This file declares the functions for editing numbers & text on the V5 Controller's
+//  screen. It is used throughout display.cpp to edit robot configuration & auton.
+//------------------------------------------------------------------------------------
+
+/**
+ * Edits a number using the controller LCD.
+ * 
+ * @param number Initial number to show in editor
+ * @param fix    Number of editable places after the decimal point
+ * @return The number, after being edited
+ */
 double editNumber(double number, int fix);
+
+/**
+ * Edits text using the controller LCD.
+ * 
+ * @param text Initial text to show in editor
+ * @return The text, after being edited
+ */
 std::string editString(std::string text);
+
+/**
+ * Selects an option out of a list of options on the controller LCD.
+ * 
+ * @param list List of all selectable options
+ * @param idx  Index of first shown option
+ * @return Index selected by user
+ */
 int selectOption(std::initializer_list<std::string> list, int idx);
