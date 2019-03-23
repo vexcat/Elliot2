ef259d6846e304459e2d3c9ffb19d12947ed0ad4
Gabriel Pulido <autodeskgato@gmail.com>
2019-03-23 09:04:07 -0700

    Make publish_docs 85x faster

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 doxygen elliot2-docs.cfg
-mv html botdocsnew
-scp -r botdocsnew root@ungato.tk:/var/www/html/ungato.tk/public_html/
-ssh root@ungato.tk rm -r /var/www/html/ungato.tk/public_html/botdocs
-ssh root@ungato.tk mv /var/www/html/ungato.tk/public_html/botdocsnew /var/www/html/ungato.tk/public_html/botdocs
-mv botdocsnew html
+mv html botdocs
+rsync -avz botdocs root@ungato.tk:/var/www/html/ungato.tk/public_html/
+mv botdocs html
