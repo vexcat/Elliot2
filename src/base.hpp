e994d3e484be705f3eab39ee4185713d7d370888
Gabriel Pulido <autodeskgato@gmail.com>
2019-03-23 17:13:38 -0700

    Put BaseSettings::loadState() into base.hpp

--- src/base.hpp
+++ src/base.hpp
@@ -26,7 +26,26 @@ class BaseSettings {
         };
     }
     public:
-    void loadState();
+    void loadState() {
+        auto dist = loadGains("dist");
+        auto angle = loadGains("angle");
+        auto turn = loadGains("turn");
+        base = std::unique_ptr<Elliot2CCPID>( new Elliot2CCPID(
+            TimeUtil(
+                Supplier<std::unique_ptr<AbstractTimer>>([]() { return std::make_unique<Timer>(); }),
+                Supplier<std::unique_ptr<AbstractRate >>([]() { return std::make_unique<Rate >(); }),
+                Supplier<std::unique_ptr<SettledUtil  >>([]() { return std::make_unique<SettledUtil>(std::make_unique<Timer>(), 0.0, 10.0, 325_ms); })
+            ),
+            std::make_shared<SkidSteerModel>(std::make_shared<MotorGroup>(left), std::make_shared<MotorGroup>(right), left.getEncoder(), right.getEncoder(), 200, 12000),
+            std::make_unique<IterativePosPIDController>(dist , TimeUtilFactory::create()),
+            std::make_unique<IterativePosPIDController>(angle, TimeUtilFactory::create()),
+            std::make_unique<IterativePosPIDController>(turn , TimeUtilFactory::create()),
+            AbstractMotor::gearset::green, {
+                (360 / (PI * cpiGetter())) * okapi::inch, ((cprGetter() * 2) / cpiGetter()) * okapi::inch
+            }
+        ));
+        base->startThread();
+    }
 
     void modGains(const char* name, const okapi::IterativePosPIDController::Gains& newGains) {
         data[name]["kP"] = newGains.kP;
